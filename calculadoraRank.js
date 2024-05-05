fbb9da021a2025b26c11d49261ae50b8b6dfb951
Omar Costa <37679473+omarcosta@users.noreply.github.com>
2024-05-05 15:19:00 -0300

    feat:  Calculate rank at calculadoraRank.js

---
+++ calculadoraRank.js
@@ -0,0 +1,26 @@
+function calcularRank(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (saldoVitorias >= 101) {
+        nivel = "Imortal";
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = "Lendário";
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = "Diamante";
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = "Ouro";
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = "Prata";
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        nivel = "Bronze";
+    } else {
+        nivel = "Ferro";
+    }
+
+    return `O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`;
+}
+
+// Saida
+const resultado = calcularRank(120, 25); // Valores de vitoria e derrotas
+console.log(resultado);
